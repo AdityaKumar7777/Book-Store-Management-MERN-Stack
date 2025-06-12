2c252860bbe09b4e05e3193eb7f97625758f7603
aditya kumar <aditya.kr777@gmail.com>
2025-06-12 17:05:11 +0530

    Final

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  'mongodb+srv://adityakr777:kLbw06sbmARGirue@cluster0.rnujwbb.mongodb.net/';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
